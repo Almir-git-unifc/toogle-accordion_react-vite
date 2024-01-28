c4382db9255c0a89bd7d5ff18e61aed3db0d1e61
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2024-01-28 17:16:45 -0300

    feat(react): v00.03.000 - feat(react): v00.03.000 - An example data list was created that was mapped and formatted for display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
+import './App.css'
 function App () {
   return (
     <>
       <div className='wrapper'>
-        <div className='accordion'>Some items</div>
+        <div className='accordion'>
+          {/* MAPEANDO OS DADOS   MAPEANDO OS DADOS   MAPEANDO OS DADOS */}
+          {data.map((item, i) => (
+            // eslint-disable-next-line react/jsx-key
+            <div className='item'>
+              <div className='title'>
+                <h2>{item.question}</h2>
+              </div>
+              <div className='content'>{item.answer}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
 }
+const data = [
+  {
+    question: 'Question 1',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus nibh  augue, dignissim convallis dolor rutrum ut. Nunc et finibus leo, in varius tortor. Pellentesque vel justo eget tortor volutpat bibendum. Nulla id cursus tellus, ac pulvinar elit. Aenean id mauris a dui egestas vehicula eu id ex. Fusce accumsan, diam vitae euismod commodo, ligula magna lacinia arcu, ac elementum tellus turpis id leo. Nunc et est tempor sapien consequat.'
+  },
+  {
+    question: 'Question 2',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus nibh augue, dignissim convallis dolor rutrum ut. Nunc et finibus leo, in varius tortor.  Pellentesque vel justo eget tortor volutpat bibendum. Nulla id cursus tellus, ac  pulvinar elit. Aenean id mauris a dui egestas vehicula eu id ex. Fusce accumsan, diam vitae euismod commodo, ligula magna lacinia arcu, ac elementum tellus turpis id leo. Nunc et est tempor sapien consequat.'
+  },
+  {
+    question: 'Question 3',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus nibh augue, dignissim convallis dolor rutrum ut. Nunc et finibus leo, in varius tortor.  Pellentesque vel justo eget tortor volutpat bibendum. Nulla id cursus tellus, ac  pulvinar elit. Aenean id mauris a dui egestas vehicula eu id ex. Fusce accumsan, diam vitae euismod commodo, ligula magna lacinia arcu, ac elementum tellus turpis id leo. Nunc et est tempor sapien consequat.'
+  },
+  {
+    question: 'Question 4',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus nibh augue, dignissim convallis dolor rutrum ut. Nunc et finibus leo, in varius tortor.  Pellentesque vel justo eget tortor volutpat bibendum. Nulla id cursus tellus, ac  pulvinar elit. Aenean id mauris a dui egestas vehicula eu id ex. Fusce accumsan, diam vitae euismod commodo, ligula magna lacinia arcu, ac elementum tellus turpis id leo. Nunc et est tempor sapien consequat.'
+  }
+]
 export default App
